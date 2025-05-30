85f68353f01bbe40777403bcfe1aae378a0c3105
Yassen Dimchov <yasen.s.dimchov.2021@elsys-bg.org>
2025-05-31 00:08:40 +0300

    finished splash page.

--- chatly/lib/main.dart
+++ chatly/lib/main.dart
@@ -3,9 +3,35 @@ import 'package:flutter/material.dart';
 //Packages
 import 'package:firebase_analytics/firebase_analytics.dart';
 
+//Services
+import '../services/navigation_service.dart';
+
 //Pages
 import './pages/splash_page.dart';
 
 void main() {
-  runApp(SplashPage(key: UniqueKey(), onInitializationComplete: () {}));
+  runApp(
+    SplashPage(
+      key: UniqueKey(),
+      onInitializationComplete: () {
+        runApp(MainApp());
+      },
+    ),
+  );
+}
+
+class MainApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chatly',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color.fromRGBO(36, 35, 49, 1.0),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: Color.fromRGBO(30, 29, 37, 1.0),
+        ),
+      ),
+      // navigatorKey: NavigationService.navigatorKey,
+    );
+  }
 }
--- chatly/lib/pages/splash_page.dart
+++ chatly/lib/pages/splash_page.dart
@@ -26,7 +26,9 @@ class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
     super.initState();
-    _setup().then((_) => widget.onInitializationComplete());
+    Future.delayed(Duration(seconds: 1)).then((_) {
+      _setup().then((_) => widget.onInitializationComplete());
+    });
   }
 
   @override
