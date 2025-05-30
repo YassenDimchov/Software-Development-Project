20d5d043de7eb59f86cdaf94a2605741b400c3a1
Yassen Dimchov <yasen.s.dimchov.2021@elsys-bg.org>
2025-05-30 23:50:28 +0300

    implemented cloud storage service

--- chatly/lib/pages/splash_page.dart
+++ chatly/lib/pages/splash_page.dart
@@ -7,6 +7,7 @@ import 'package:get_it/get_it.dart';
 //Services
 import '../services/navigation_service.dart';
 import '../services/media_service.dart';
+import '../services/cloud_storage_service.dart';
 
 class SplashPage extends StatefulWidget {
   final VoidCallback onInitializationComplete;
@@ -60,5 +61,6 @@ class _SplashPageState extends State<SplashPage> {
   void _registerServices() {
     GetIt.instance.registerSingleton<NavigationService>(NavigationService());
     GetIt.instance.registerSingleton<MediaService>(MediaService());
+    GetIt.instance.registerSingleton<CloudStorageService>(CloudStorageService());    
   }
 }
