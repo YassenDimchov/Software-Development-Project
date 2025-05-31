3ca4fc08e21bc487e2e0e8804fb91853ed0a4f7a
Yassen Dimchov <yasen.s.dimchov.2021@elsys-bg.org>
2025-06-01 00:28:04 +0300

    implemented the structure and the image picker for register page.

---
+++ chatly/lib/pages/register_page.dart
@@ -0,0 +1,87 @@
+//Packages
+import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:get_it/get_it.dart';
+import 'package:provider/provider.dart';
+
+//Services
+import '../services/media_service.dart';
+import '../services/database_serivce.dart';
+import '../services/cloud_storage_service.dart';
+
+//Widgets
+import '../widgets/custom_input_fields.dart';
+import '../widgets/rounded_button.dart';
+import 'package:chatly/widgets/rounded_image.dart';
+
+//Providers
+import '../providers/authentiaction_provider.dart';
+
+class RegisterPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _RegisterPageState();
+  }
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  late double _deviceHeight;
+  late double _deviceWidth;
+
+  PlatformFile? _profileImage;
+
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return _buildUI();
+  }
+
+  Widget _buildUI() {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: _deviceWidth * 0.03,
+          vertical: _deviceHeight * 0.02,
+        ),
+        height: _deviceHeight * 0.98,
+        width: _deviceWidth * 0.97,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [_profileImageField()],
+        ),
+      ),
+    );
+  }
+
+  Widget _profileImageField() {
+    return GestureDetector(
+      onTap: () {
+        GetIt.instance.get<MediaService>().pickImageFromLibrary().then((_file) {
+          setState(() {
+            _profileImage = _file;
+          });
+        });
+      },
+      child: () {
+        if (_profileImage != null) {
+          return RoundedImageFile(
+            key: UniqueKey(),
+            image: _profileImage!,
+            size: _deviceHeight * 0.15,
+          );
+        } else {
+          return RoundedImageNetwork(
+            key: UniqueKey(),
+            imagePath:
+                "https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg",
+            size: _deviceHeight * 0.15,
+          );
+        }
+      }(),
+    );
+  }
+}
