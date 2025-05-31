9044c55f43cad07e207cd36162a440cfdde17f65
Yassen Dimchov <yasen.s.dimchov.2021@elsys-bg.org>
2025-06-01 00:39:53 +0300

    Finished the UI of the register page

--- chatly/lib/pages/register_page.dart
+++ chatly/lib/pages/register_page.dart
@@ -28,8 +28,13 @@ class _RegisterPageState extends State<RegisterPage> {
   late double _deviceHeight;
   late double _deviceWidth;
 
+  String? _email;
+  String? _password;
+  String? _name;
+
   PlatformFile? _profileImage;
 
+  final _registerFormKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     _deviceHeight = MediaQuery.of(context).size.height;
@@ -51,7 +56,14 @@ class _RegisterPageState extends State<RegisterPage> {
           mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: [_profileImageField()],
+          children: [
+            _profileImageField(),
+            SizedBox(height: _deviceHeight * 0.05),
+            _registerForm(),
+            SizedBox(height: _deviceHeight * 0.05),
+            _registerButton(),
+            SizedBox(height: _deviceHeight * 0.02),
+          ],
         ),
       ),
     );
@@ -82,6 +94,62 @@ class _RegisterPageState extends State<RegisterPage> {
           );
         }
       }(),
+    );
+  }
+
+  Widget _registerForm() {
+    return Container(
+      height: _deviceHeight * 0.35,
+      child: Form(
+        key: _registerFormKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CustomTextFormField(
+              onSaved: (_value) {
+                setState(() {
+                  _name = _value;
+                });
+              },
+              regEx: r".{8,}",
+              hintText: "Name",
+              obscureText: false,
+            ),
+            CustomTextFormField(
+              onSaved: (_value) {
+                setState(() {
+                  _email = _value;
+                });
+              },
+              regEx:
+                  r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+              hintText: "Email",
+              obscureText: false,
+            ),
+            CustomTextFormField(
+              onSaved: (_value) {
+                setState(() {
+                  _password = _value;
+                });
+              },
+              regEx: r".{8,}",
+              hintText: "Password",
+              obscureText: false,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _registerButton() {
+    return RoundedButton(
+      name: 'Register',
+      height: _deviceHeight * 0.065,
+      width: _deviceWidth * 0.65,
+      onPressed: () async {},
     );
   }
 }
