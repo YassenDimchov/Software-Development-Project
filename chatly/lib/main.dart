78802c894f2405392b5207f03934d4ab192a3845
Yassen Dimchov <yasen.s.dimchov.2021@elsys-bg.org>
2025-05-31 19:42:48 +0300

    created the basic structure of the login page and implemented routes

--- chatly/lib/main.dart
+++ chatly/lib/main.dart
@@ -8,6 +8,7 @@ import '../services/navigation_service.dart';
 
 //Pages
 import './pages/splash_page.dart';
+import './pages/login_page.dart';
 
 void main() {
   runApp(
@@ -31,7 +32,9 @@ class MainApp extends StatelessWidget {
           backgroundColor: Color.fromRGBO(30, 29, 37, 1.0),
         ),
       ),
-      // navigatorKey: NavigationService.navigatorKey,
+      navigatorKey: NavigationService.navigatorKey,
+      initialRoute: '/login',
+      routes: {'/login': (BuildContext _context) => LoginPage()},
     );
   }
 }
