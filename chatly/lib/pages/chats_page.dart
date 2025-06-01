52a53770b475d35516c603b4429a109dbc3b8c32
Yassen Dimchov <yasen.s.dimchov.2021@elsys-bg.org>
2025-06-01 19:23:47 +0300

    finished the top bar.

--- chatly/lib/pages/chats_page.dart
+++ chatly/lib/pages/chats_page.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:get_it/get_it.dart';
 
+//Providers
+import '../providers/authentiaction_provider.dart';
+
+//Widgets
+import '../widgets/top_bar.dart';
+
 class ChatsPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -11,12 +17,43 @@ class ChatsPage extends StatefulWidget {
 }
 
 class _ChatsPageState extends State<ChatsPage> {
+  late double _deviceHeight;
+  late double _deviceWidth;
+
+  late AuthentiactionProvider _auth;
+
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    _auth = Provider.of<AuthentiactionProvider>(context);
     return _buildUI();
   }
 
   Widget _buildUI() {
-    return Scaffold(backgroundColor: Colors.red);
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: _deviceWidth * 0.03,
+        vertical: _deviceHeight * 0.02,
+      ),
+      height: _deviceHeight * 0.98,
+      width: _deviceWidth * 0.97,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          TopBar(
+            "Chats",
+            primaryAction: IconButton(
+              onPressed: () {
+                _auth.logout();
+              },
+              icon: Icon(Icons.logout, color: Color.fromRGBO(0, 82, 218, 1.0)),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
