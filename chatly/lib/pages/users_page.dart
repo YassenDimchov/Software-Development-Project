d98c8a17207b2840805cc6808f241dfaa7abeb3b
Yassen Dimchov <yasen.s.dimchov.2021@elsys-bg.org>
2025-06-04 17:23:29 +0300

    created the top bar and the search field

--- chatly/lib/pages/users_page.dart
+++ chatly/lib/pages/users_page.dart
@@ -3,6 +3,18 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:get_it/get_it.dart';
 
+//Providers
+import '../providers/authentiaction_provider.dart';
+
+//Widgets
+import '../widgets/top_bar.dart';
+import '../widgets/custom_input_fields.dart';
+import '../widgets/custom_list_view_tiles.dart';
+import '../widgets/rounded_button.dart';
+
+//Models
+import '../models/chat_user.dart';
+
 class UsersPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -11,12 +23,51 @@ class UsersPage extends StatefulWidget {
 }
 
 class _UsersPageState extends State<UsersPage> {
+  late double _deviceWidth;
+  late double _deviceHeight;
+
+  late AuthentiactionProvider _auth;
+
+  final TextEditingController _searchFieldTextEditingController =
+      TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    _auth = Provider.of<AuthentiactionProvider>(context);
     return _buildUI();
   }
 
   Widget _buildUI() {
-    return Scaffold(backgroundColor: Colors.green);
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: _deviceWidth * 0.03,
+        vertical: _deviceHeight * 0.02,
+      ),
+      height: _deviceHeight * 0.98,
+      width: _deviceWidth * 0.97,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          TopBar(
+            "Users",
+            primaryAction: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.logout, color: Color.fromRGBO(0, 82, 218, 1.0)),
+            ),
+          ),
+          CustomTextField(
+            onEditingComplete: (_value) {},
+            hintText: "Search...",
+            obscureText: false,
+            controller: _searchFieldTextEditingController,
+            icon: Icons.search,
+          ),
+        ],
+      ),
+    );
   }
 }
